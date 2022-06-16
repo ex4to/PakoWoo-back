b565329c338240a62b7da2600f0ba9c528481cfa
secretDzen <turretbox@gmail.com>
2022-06-16 23:06:48 +0700

    feat: new endpoints

---
+++ queues.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    id: 1,
+    subject: "Моделирование",
+    date: "20/05",
+    participants: ["Kostyan", "Vasiliy"],
+  },
+  {
+    id: 2,
+    subject: "Английский",
+    date: "21/05",
+    participants: ["Andrew", "Sergei", "Party Maker"],
+  },
+  {
+    id: 3,
+    subject: "Английский",
+    date: "15/05",
+    participants: ["Balakin", "Павел Попов"],
+  },
+];
--- server.js
+++ server.js
@@ -1,17 +1,56 @@
-const express = require("express");
+import express, { json } from "express";
+import queues from "./queues.js";
+import subjects from "./subjects.js";
 const app = express();
-
-app.use(express.json());
-
 const port = 8080;
 
-app.get("/api", (req, res) => {
-  res.send("Hello World");
+app.use(json());
+
+app.get("/subjects/all", (req, res) => {
+  res.statusCode = 200;
+  res.json(JSON.stringify(subjects));
 });
 
-app.post("/api", (req, res) => {
+app.get("/queues/all", (req, res) => {
+  res.statusCode = 200;
+  res.json(JSON.stringify(queues));
+});
+
+app.post("/subjects/add", (req, res) => {
   console.log(req.body);
   res.send("Catch it");
+});
+
+app.post("/queues/add", (req, res) => {
+  const baza = queues.findIndex(
+    (a) => a.subject === req.body.subject && a.date === req.body.date
+  );
+  if (baza === -1) {
+    const response = {
+      id: Date.now(),
+      subject: req.body.subject,
+      date: req.body.date,
+      participants: req.body.participants,
+    };
+    queues.push(response);
+  }
+  res.send("Catch it");
+});
+
+app.post("/queues/insert", (req, res) => {
+  const idx = queues.findIndex((e) => e.id === req.body.selectedItem.id);
+  queues[idx].participants.push(req.body.userName);
+  res.send("Added to queue");
+});
+
+app.post("/queues/delete", (req, res) => {
+  const idx = queues.findIndex((e) => e.id === req.body.selectedItem.id);
+  queues[idx].participants = queues[idx].participants.filter(
+    (e) => e !== req.body.userName
+  );
+  if (queues[idx].participants.length < 1)
+    queues = queues.filter((e, index) => queues[index] !== queues[idx]);
+  res.send("Deleted to queue");
 });
 
 app.listen(port, () => {
